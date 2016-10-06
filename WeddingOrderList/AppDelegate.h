5f09239fda81452e4033fd50ca7b061573f94b17
kingleekong <653218086@qq.com>
2016-10-06 11:07:02 +0800

    Initial Commit

---
+++ WeddingOrderList/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WeddingOrderList
+//
+//  Created by kong on 16/10/6.
+//  Copyright © 2016年 kong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WeddingOrderList/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WeddingOrderList
+//
+//  Created by kong on 16/10/6.
+//  Copyright © 2016年 kong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
